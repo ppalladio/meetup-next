6ca711891e0a0f23c049877095051aa48966c61d
ppalladio <nine.slide@gmail.com>
2022-11-04 22:05:24 +0100

    add custom component and render it in pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image="https://images.pexels.com/photos/13563689/pexels-photo-13563689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            title="first meetup"
+            description="Lorem ipsum dolor sit amet,"
+        />
+    );
+}
+
+export default MeetupDetails;
